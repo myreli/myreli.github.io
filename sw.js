2d683a836e2e341152a668200a4357a51a34e412
myreli <borbamyreli@gmail.com>
2018-08-15 20:18:04 -0300

    add service worker

---
+++ sw.js
@@ -0,0 +1,43 @@
+//adicionar ao cache todos os arquivos estáticos
+var CACHE_NAME = 'static-v1';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './assets/css/styles.css',
+                './js/jquery.min.js',
+                './js/script.js',
+                './js/skel.min.js',
+                './js/util.js',
+                './manifest.js',
+                './images/bg.jpg',
+            ]);
+        })
+
+    //Ao ativar atualiza o cache se necessário
+    self.addEventListener('activate', function activator(event) {
+            event.waitUntil(caches.keys().then(function (keys) {
+                return Promise.all(keys.filter(function (key) {
+
+                    return key.indexOf(CACHE_NAME) !== 0;
+                }).map(function (key) {
+
+                    return caches.delete(key);
+                })
+                );
+            })
+            );
+        });
+
+    //pegar o que for solicitado do cache, e se ele não existir fazer um
+    request
+    self.addEventListener('fetch', function (event) {
+        event.respondWith(
+            caches.match(event.request).then(function (cachedResponse) {
+                return cachedResponse || fetch(event.request);
+            })
+        );
+    });
