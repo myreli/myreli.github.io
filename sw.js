b97eab1259b2105d2cb8b61ad72d0c8998e6ff1a
myreli <borbamyreli@gmail.com>
2018-08-15 20:26:45 -0300

    fix sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //adicionar ao cache todos os arquivos estáticos
 var CACHE_NAME = 'static-v1';
 
-self.addEventListener('install', function (event) {
+this.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll([
@@ -16,28 +16,29 @@ self.addEventListener('install', function (event) {
                 './images/bg.jpg',
             ]);
         })
+    )
+});
 
-    //Ao ativar atualiza o cache se necessário
-    self.addEventListener('activate', function activator(event) {
-            event.waitUntil(caches.keys().then(function (keys) {
-                return Promise.all(keys.filter(function (key) {
+//Ao ativar atualiza o cache se necessário
+this.addEventListener('activate', function activator(event) {
+    event.waitUntil(caches.keys().then(function (keys) {
+        return Promise.all(keys.filter(function (key) {
 
-                    return key.indexOf(CACHE_NAME) !== 0;
-                }).map(function (key) {
+            return key.indexOf(CACHE_NAME) !== 0;
+        }).map(function (key) {
 
-                    return caches.delete(key);
-                })
-                );
-            })
-            );
-        });
-
-    //pegar o que for solicitado do cache, e se ele não existir fazer um
-    request
-    self.addEventListener('fetch', function (event) {
-        event.respondWith(
-            caches.match(event.request).then(function (cachedResponse) {
-                return cachedResponse || fetch(event.request);
-            })
+            return caches.delete(key);
+        })
         );
-    });
+    })
+    );
+});
+
+//pegar o que for solicitado do cache, e se ele não existir fazer um request
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (cachedResponse) {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
